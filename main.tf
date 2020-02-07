997654a051c11b3f76f515884dbb4e4cd8e970a4
Vedran Maricevic <wex.alpha@gmail.com>
2020-02-07 10:44:41 +0100

    Removed duplicate EC2 permission

--- main.tf
+++ main.tf
@@ -131,8 +131,7 @@ data "aws_iam_policy_document" "permissions" {
       "ec2:DescribeNetworkInterfaces",
       "ec2:DeleteNetworkInterface",
       "ec2:DescribeDhcpOptions",
-      "ec2:CreateNetworkInterface",
-      "ec2:DeleteNetworkInterface"
+      "ec2:CreateNetworkInterface"
     ]
 
     effect = "Allow"
